27823438cbb4e04951b11cdd11fc4ae0899570eb
Ubuntu <ubuntu@ip-172-31-43-188.ec2.internal>
2019-07-03 23:05:16 +0000

    initial

--- ruby.rb
+++ ruby.rb
@@ -4,45 +4,71 @@
 
 tempArray = [1, 2, 6, 9, 3, 21]
 
-# your ruby loop here
+tempArray = [1, 2, 6, 9, 3, 21]
+tempArray.each do |a|
+  puts a*2
+  end
+
+puts tempArray.map { |a| a * 2}
 
 
 #### 2. From all the built in Ruby methods we've seen in class this week, choose three that you think are particularly helpful and create examples to show how they work.
 
-# your three built in ruby methods
+# .lenght
+tempArray = [1, 2, 6, 9, 3, 21]
+puts tempArray.length
+# .reverse
+puts tempArray.reverse
+# .include?
+puts tempArray.include?1
 
 
 #### 3. Create a method that takes in a sentence and returns a new sentence with the first letter of each word capitalized.
 
 sentence = "hello there, how are you?"
-expected output = "Hello There, How Are You?"
+def capsent (str)
+  newArr = str.split(' ')
+  newArr.map! { |s|
+    s.capitalize }
+    newArr.join(' ')
+end
+puts capsent (sentence)
+
+# expected output = "Hello There, How Are You?"
 
 #### 4.  Create a method that takes in a string and returns a new string with all the vowels removed. HINT:  there's a built in string method for this
 
 no_vowels = "I have no vowels"
 expected output = " hv n vwls"
+no_vowels = "I have no vowels"
+def noVowels (str)
+  str.delete("aeiouAEIOU")
+end
+puts noVowels(no_vowels)
+
+# expected output = " hv n vwls"
 
 
 #### 5. Look at this horrible ruby code, and fix it to be good ruby code.
 
-``` ruby
-class example
+class Example
   def initialize(day)
     @day=day
   end
 
-  def Say_hi
-    if(day == "Friday"){
+  def say_hi
+    if @day == 'Friday'
       puts "TGIF!"
-    }
-    elseif(day == "Monday"){
+
+    elsif @day == 'Monday'
       puts "Its monday again"
-    }
+
     else
-      puts "another day"
+      puts 'another day'
+    end
   end
 end
-```
+
 #### 6a.  Create a class called Animal that initializes with a color.  Create a method in the class called legs that returns 4.
 
 # your class here
